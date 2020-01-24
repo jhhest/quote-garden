adeeeaf2d93f81447a9d75d0461b0c2e95ad09ec
jhhest <janvanhest@outlook.com>
2020-01-24 22:25:00 +0100

    Added componentDidMount. Data is now fetched and wil add an array with results and set the loading to false. Also implemented a visual check if data is loading. When loading the text loading... appears.

--- src/Components/QuoteSearcher/index.jsx
+++ src/Components/QuoteSearcher/index.jsx
@@ -3,36 +3,31 @@ import Quote from "./Quote";
 
 export class QuoteSearcher extends Component {
   state = {
-    quotes: [
-      {
-        _id: "5d91b45d9980192a317c8acc",
-        quoteText:
-          "Notice that the stiffest tree is most easily cracked, while the bamboo or willow survives by bending with the wind.",
-        quoteAuthor: "Bruce Lee"
-      },
-      {
-        _id: "5d91b45d9980192a317c8abe",
-        quoteText:
-          "Give me six hours to chop down a tree and I will spend the first four sharpening the axe.",
-        quoteAuthor: "Abraham Lincoln"
-      },
-      {
-        _id: "5d91b45d9980192a317c8955",
-        quoteText:
-          "Good timber does not grow with ease; the stronger the wind, the stronger the trees.",
-        quoteAuthor: "J. Willard Marriott"
-      }
-    ]
+    loading: true,
+    quotes: []
   };
 
-  showQuotes = (quotesArray) => quotesArray.map( quote => <Quote text={quote.quoteText}author={quote.quoteAuthor}/>)
+  componentDidMount() {
+    fetch("https://quote-garden.herokuapp.com/quotes/search/tree")
+      .then(resolve => resolve.json())
+      .then(fetchedQuotes =>
+        this.setState({ quotes: fetchedQuotes.results, loading: false })
+      );
+  }
+
+  showQuotes = quotesArray =>
+    quotesArray.map(quote => (
+      <Quote text={quote.quoteText} author={quote.quoteAuthor} />
+    ));
 
   render() {
+    const { loading, quotes } = this.state;
+
     return (
       <Fragment>
-        <h1>QuoteSearcher.</h1>
-        {this.showQuotes(this.state.quotes)}
-        <h1>Below an Example of one Quote Element:</h1>
+        <h1 style={{ textAlign: "center" }}>QuoteSearcher.</h1>
+        {loading && <p>Loading...</p>}
+        {!loading && this.showQuotes(quotes)}
       </Fragment>
     );
   }

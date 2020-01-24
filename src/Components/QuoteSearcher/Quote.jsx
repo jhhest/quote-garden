b143baae5119772907e284c71dd75dc7afc10dcc
jhhest <janvanhest@outlook.com>
2020-01-24 21:59:50 +0100

    Added Google Font and changed some basic styling

--- src/Components/QuoteSearcher/Quote.jsx
+++ src/Components/QuoteSearcher/Quote.jsx
@@ -4,14 +4,8 @@ export class Quote extends Component {
   render() {
     return (
       <Fragment>
-        <blockquote
-          style={{
-            backgroundColor: "lightblue",
-            margin: "2rem",
-            padding: "1rem"
-          }}
-        >
-          <p style={{ marginTop: "0px" }}>{this.props.text}</p>
+        <blockquote>
+          <p>{this.props.text}</p>
           <footer>
             By: <em>{this.props.author}</em>
           </footer>

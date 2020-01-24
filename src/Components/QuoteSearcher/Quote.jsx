9d3d7cb23d960c9b9ffc7c9b7b19b1172ec07494
jhhest <janvanhest@outlook.com>
2020-01-24 23:18:41 +0100

    added buttons in quote component and change styling of blockquote with codesnippet.

--- src/Components/QuoteSearcher/Quote.jsx
+++ src/Components/QuoteSearcher/Quote.jsx
@@ -7,7 +7,7 @@ export class Quote extends Component {
         <blockquote>
           <p>{this.props.text}</p>
           <footer>
-            By: <em>{this.props.author}</em>
+            By: <em>{this.props.author}</em> <button><i class="fa fa-thumbs-up"></i></button><button><i class="fa fa-thumbs-down"></i></button>
           </footer>
         </blockquote>
       </Fragment>

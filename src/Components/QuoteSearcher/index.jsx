b1f741d93b32ca0d70c1793eadb9cb440fbaf55f
jhhest <janvanhest@outlook.com>
2020-01-25 13:12:19 +0100

    Added setDislike and setLike arrow functions. when a button is triggered in quote an onclick event triggers these functions that are defined in index.js//QuoteSearcher will alter the state in the QuoteSearcher Component.

--- src/Components/QuoteSearcher/index.jsx
+++ src/Components/QuoteSearcher/index.jsx
@@ -11,7 +11,17 @@ export class QuoteSearcher extends Component {
     fetch("https://quote-garden.herokuapp.com/quotes/search/tree")
       .then(resolve => resolve.json())
       .then(fetchedQuotes =>
-        this.setState({ quotes: fetchedQuotes.results, fetching: false })
+        this.setState({
+          quotes: fetchedQuotes.results.map(quote => {
+            return {
+              id: quote._id,
+              quoteAuthor: quote.quoteAuthor,
+              quoteText: quote.quoteText,
+              numLike: null
+            };
+          }),
+          fetching: false
+        })
       );
   }
 
@@ -20,19 +30,66 @@ export class QuoteSearcher extends Component {
       <Quote
         text={quote.quoteText}
         author={quote.quoteAuthor}
-        key={quote._id}
+        key={quote.id}
+        setLike={() => this.setLike(quote.id)}
+        setDislike={() => this.setDislike(quote.id)}
+        numLike={quote.numLike}
       />
     ));
 
+  setLike = id => {
+    console.log("this is id:", id);
+
+    const copyOfState = { ...this.state };
+    console.log("this is a copy of state", copyOfState);
+    const copyOfQuotesArr = [...this.state.quotes];
+    console.log(
+      "this is a copy quotes array that is in state",
+      copyOfQuotesArr
+    );
+
+    const mutateFetchedQuotes = copyOfQuotesArr.map(quote =>
+      quote.id === id ? { ...quote, numLike: true } : quote
+    );
+    console.log(
+      "this is a copy quotes array that is in state",
+      mutateFetchedQuotes
+    );
+    this.setState({ quotes: mutateFetchedQuotes });
+      //Works!
+  };
+
+  setDislike = (id) => {
+    console.log("this is id:", id);
+
+    const copyOfState = { ...this.state };
+    console.log("this is a copy of state", copyOfState);
+    const copyOfQuotesArr = [...this.state.quotes];
+    console.log(
+      "this is a copy quotes array that is in state",
+      copyOfQuotesArr
+    );
+
+    const mutateFetchedQuotes = copyOfQuotesArr.map(quote =>
+      quote.id === id ? { ...quote, numLike: false } : quote
+    );
+    console.log(
+      "this is a copy quotes array that is in state",
+      mutateFetchedQuotes
+    );
+    this.setState({ quotes: mutateFetchedQuotes });
+      //Works!
+  };
+
   render() {
-    const { loading, quotes } = this.state;
+    const { fetching, quotes } = this.state;
 
     return (
       <Fragment>
         <h1 style={{ textAlign: "center" }}>QuoteSearcher.</h1>
-        {loading && <p>Loading...</p>}
+        {fetching && <p>Loading...</p>}
         <section id="center-quotes">
-          {!loading && this.showQuotes(quotes)}
+          {!fetching && this.showQuotes(quotes)}
         </section>
       </Fragment>
     );
